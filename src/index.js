36224e131b35266c71964b3c3f47387a54102347
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2017-07-16 13:52:33 -0600

    Fetched data from our API

--- src/index.js
+++ src/index.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 
 const API_KEY = 'AIzaSyDky8aONBX8Jep3OnK9WnTFyElj7ew5Ass';
 // Creating a new component to produce HTML
-// This is like a factory producing instances of App - a functional based component
-const App = () => {
-    return (
-        <div>
-            <SearchBar />
-        </div>
-    );
+// This is like a factory producing instances of App
+class App extends Component {
+    constructor(props){
+        super(props);
+
+        this.state = { videos: [] };
+                                                    // data
+        // Automatically populate data by putting this function in the constructor
+        YTSearch({ key: API_KEY, term: 'surfing' }, (videos) => {
+            this.setState({ videos }); // syntactic sugar, when the key and the value are the same variable name
+            // this.setState({ videos: videos});
+        });
+    }
+    render() {
+        return (
+            <div>
+                <SearchBar />
+            </div>
+        );
+    }
 }
 
 // Generate the component's HTML

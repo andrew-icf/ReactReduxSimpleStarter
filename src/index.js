97b823fc064039e26ee10a82f7d5f92c52c18c96
Andrew Vandermeer <innerchaosfitness@yahoo.com>
2017-07-20 22:21:06 -0600

    Added video detail with title and description

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 const API_KEY = 'AIzaSyDky8aONBX8Jep3OnK9WnTFyElj7ew5Ass';
 // Creating a new component to produce HTML
@@ -23,6 +24,7 @@ class App extends Component {
         return (
             <div>
                 <SearchBar />
+                <VideoDetail video={this.state.videos[0]}/>
                 <VideoList videos={this.state.videos} />
             </div>
         );
